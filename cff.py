b8a22c78fe63373ee7cc3621d5f9800b1c26f616
brunoafigueira <brunoafigueira@gmail.com>
2025-04-14 20:57:57 -0300

    Update cff.py

--- cff.py
+++ cff.py
@@ -17,7 +17,7 @@ authenticator = stauth.Authenticate(
     config['cookie']['name'],
     config['cookie']['key'],
     config['cookie']['expiry_days'],
-    config['preauthorized']
+    config['preauthorized_emails']
 )
 
 # ======================================
